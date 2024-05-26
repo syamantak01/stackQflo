87cc77854e2433ccdcc76dbc7e63ba7c69b9e62b
Syamantak Das <dasyamantak@gmail.com>
2024-05-26 15:23:57 +0530

    Project setup

---
+++ src/test/java/com/dexcode/stackQflo/StackQfloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dexcode.stackQflo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StackQfloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
